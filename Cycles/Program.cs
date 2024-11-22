f7820cd505f308ffccbee09557bf8bc878fa64e2
Vadym <vadym1927dk@gmail.com>
2024-11-22 12:15:39 +0200

    4 задачі

--- Cycles/Program.cs
+++ Cycles/Program.cs
@@ -66,27 +66,72 @@
             //числа від 1 до заданого користувачем числа. Просте число -
             //це число, яке ділиться лише на 1 і на себе.
 
-            Console.WriteLine("Please write your max value:");
-            string strMaxValue = Console.ReadLine();
-            bool thisIsAPrimeNumber = true; //булева зміна для перевірки числа на простосту
+            //Console.WriteLine("Please write your max value:");
+            //string strMaxValue = Console.ReadLine();
+            //bool thisIsAPrimeNumber = true; //булева зміна для перевірки числа на простоту
 
-            if (int.TryParse(strMaxValue, out int maxValue) && maxValue > 0)
-            {
-                Console.WriteLine($"Prime numbers up to {maxValue}:");
-                for (int i = 2; i <= maxValue; i++)
-                {
-                    for (int j = 2; j <= Math.Sqrt(i); j++)
-                    {
-                        if (i % j == 0) thisIsAPrimeNumber = false;                   
-                    }
-                    if (thisIsAPrimeNumber) { Console.Write(i + " "); }
-                    thisIsAPrimeNumber = true;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Error ******************************************* Error");
-            }                     
+            //if (int.TryParse(strMaxValue, out int maxValue) && maxValue > 0)
+            //{
+            //    Console.WriteLine($"Prime numbers up to {maxValue}:");
+            //    for (int i = 2; i <= maxValue; i++)
+            //    {
+            //        //перевірка дільників до корня з i
+            //        for (int j = 2; j <= Math.Sqrt(i); j++)
+            //        {
+            //            //якщо знайдено дільник, число не є простим
+            //            if (i % j == 0) thisIsAPrimeNumber = false;
+            //        }
+            //        if (thisIsAPrimeNumber) { Console.Write(i + " "); }
+            //        thisIsAPrimeNumber = true; 
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error ******************************************* Error");
+            //}
+
+            //4. Напишіть програму, яка перевіряє пароль користувача.
+            //Пароль вважається прийнятним, якщо він містить принаймні
+            //8 символів, з яких принаймні один символ - це цифра, а
+            //також принаймні один символ - це спеціальний символ
+            //(наприклад, !, @, # тощо).
+
+            //string password = string.Empty;
+            //bool badPassword = true;
+            //while (badPassword)
+            //{
+            //    sbyte minLength = 8; //мінімальна довжина
+            //    bool oneDigit = false; //перевірка на цифру
+            //    bool oneSpecialSymbol = false; //перевірка на спец.символ
+
+            //    Console.WriteLine("Please create your password");
+            //    password = Console.ReadLine();
+
+            //    //хз як це робити без масивів, тому буду користуватися ними
+            //    for (int i = 0; i < password.Length; i++) // проходимо по символам рядка
+            //    {
+            //        //в char є зручні методи перевірки символів
+            //        //https://learn.microsoft.com/en-us/dotnet/api/system.char.isdigit?view=net-9.0
+            //        if (char.IsDigit(password[i])) oneDigit = true;
+            //        if (!char.IsLetterOrDigit(password[i])) oneSpecialSymbol = true;
+                   
+            //    }
+
+
+            //    //умова виходу з циклу
+            //    if (minLength <= password.Length && oneDigit && oneSpecialSymbol)
+            //    {
+            //        badPassword = false;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Incorrect password. Please try again.");
+            //    }               
+            //}
+            //Console.WriteLine("Password has been created");
+
+            //5. 
+
         }
     }
 }
